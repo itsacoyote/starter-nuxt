8e90d2b12a16a76f79106df50100f9b859d1caec
itsacoyote <brina.heath@gmail.com>
2024-12-10 20:41:12 -0500

    setup Nuxt with eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+// @ts-check
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(
+  // Your custom configs here
+  {
+    plugins: { "simple-import-sort": simpleImportSort },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "sort-imports": "off",
+      "vue/multi-word-component-names": "off",
+      "object-curly-newline": ["error", {
+        ObjectExpression: {
+          multiline: true,
+          minProperties: 3,
+        },
+        ObjectPattern: {
+          multiline: true,
+          minProperties: 3,
+        },
+        ImportDeclaration: {
+          multiline: true,
+          minProperties: 5,
+        },
+        ExportDeclaration: {
+          multiline: true,
+          minProperties: 3,
+        },
+      }],
+      "object-property-newline": ["error"],
+    },
+    settings: { "import/core-modules": ["vue-router/auto-routes"] },
+  }
+)
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint'],
+  eslint: {
+    config: {
+      stylistic: {
+        indent: 2,
+        quotes: "double",
+        semi: true,
+        arrowParens: true,
+        quoteProps: "as-needed",
+        braceStyle: "1tbs",
+      }
+    }
+  }
+})
